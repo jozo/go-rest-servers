685d5b0896cb2b97e9ecb068e5df8b03eb6d59b5
jozo <hi@jozo.io>
2021-02-10 23:03:12 +0100

    Add gin

--- server.go
+++ server.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 	"github.com/jozo/go-rest-servers/store"
 	"log"
-	"mime"
 	"net/http"
 	"strconv"
 	"time"
 )
 
 func main() {
-	router := mux.NewRouter()
-	router.StrictSlash(true)
+	router := gin.Default()
 	server := NewTaskServer()
 
-	router.HandleFunc("/task/", server.createTaskHandler).Methods("POST")
-	router.HandleFunc("/task/", server.getAllTasksHandler).Methods("GET")
-	router.HandleFunc("/task/{id:[0-9]+}/", server.getTaskHandler).Methods("GET")
+	router.POST("/task/", server.createTaskHandler)
+	router.GET("/task/", server.getAllTasksHandler)
+	router.GET("/task/:id/", server.getTaskHandler)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
@@ -32,66 +29,37 @@ func NewTaskServer() *taskServer {
 	return &taskServer{store: store}
 }
 
-func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling get all tasks at %s\n", req.URL.Path)
-
+func (ts *taskServer) getAllTasksHandler(c *gin.Context) {
 	allTasks := ts.store.GetAllTasks()
-	renderJSON(w, allTasks)
+	c.JSON(http.StatusOK, allTasks)
 }
 
-func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling task create at %s\n", req.URL.Path)
-
+func (ts *taskServer) createTaskHandler(c *gin.Context) {
 	type RequestTask struct {
 		Text string    `json:"text"`
 		Tags []string  `json:"tags"`
 		Due  time.Time `json:"due"`
 	}
 
-	type ResponseId struct {
-		Id int `json:"id"`
-	}
-
-	contentType := req.Header.Get("Content-Type")
-	mediatype, _, err := mime.ParseMediaType(contentType)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-
-	if mediatype != "application/json" {
-		http.Error(w, "expect application/json Content-Type", http.StatusUnsupportedMediaType)
-		return
-	}
-
-	dec := json.NewDecoder(req.Body)
-	dec.DisallowUnknownFields()
 	var rt RequestTask
-	if err := dec.Decode(&rt); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+	if err := c.ShouldBindJSON(&rt); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
 	}
 	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
-	renderJSON(w, ResponseId{Id: id})
+	c.JSON(http.StatusOK, gin.H{"Id": id})
 }
 
-func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling get task at %s\n", req.URL.Path)
+func (ts *taskServer) getTaskHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Params.ByName("id"))
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
 
-	id, _ := strconv.Atoi(mux.Vars(req)["id"])
 	task, err := ts.store.GetTask(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
+		c.String(http.StatusNotFound, err.Error())
 		return
 	}
-	renderJSON(w, task)
-}
-
-func renderJSON(w http.ResponseWriter, v interface{}) {
-	js, err := json.Marshal(v)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	c.JSON(http.StatusOK, task)
 }
